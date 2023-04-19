acd9c09047bfcbded6000ddf8cf2979f517b571d
James Munywoki <58624365+lifecode-j@users.noreply.github.com>
2023-04-19 14:32:29 +0545

    Final Commit before submission

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,5 +1,10 @@
 class Api {
   static var baseUrl = "http://mealmentor.codeswithbipin.com/api";
+  static var baseUrlLive = "http://mealmentor.codeswithbipin.com/api";
+  // If Using Laravel Valet
+  static var baseUrlLocal = "http://mealmentor.test/api";
+  // Other wise use ip address
+
   static String login = "$baseUrl/login";
   static String item = "$baseUrl/item-categories";
   static String itemByCategory = "$baseUrl/items?item_category_id";
